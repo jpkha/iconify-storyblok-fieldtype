ae9187aea63cf05ba364d9d51cae68b74a6a48dd
jpkha <jeanphilippe.kha@gmail.com>
2023-02-19 23:37:15 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,35 @@
+# Storyblok Field-Type
+
+> A Storyblok Field-Type Plugin Starter Project
+
+## Project setup
+```
+npm install
+```
+
+### Compiles and hot-reloads for development
+```
+npm run serve
+```
+
+In the browser, you will see the white screen with the following error in the console:
+```
+[Vue warn]: Error in created hook: "SyntaxError: Failed to execute 'postMessage' on 'Window': Invalid target origin 'null//app.storyblok.com' in a call to 'postMessage'."
+
+DOMException: Failed to execute 'postMessage' on 'Window': Invalid target origin 'null//app.storyblok.com' in a call to 'postMessage'.
+```
+But don't worry, this project is just a template and only works as part of the storyblok app. See: 
+https://www.storyblok.com/docs/plugins/enable-local-dev-mode
+
+### Compiles and minifies for production
+```
+npm run build
+```
+
+### Lints and fixes files
+```
+npm run lint
+```
+
+### Customize configuration
+https://www.storyblok.com/docs/Guides/Creating-a-field-type-plugin#how-to-develop-plugins-locally
---
+++ src/Plugin.vue
@@ -0,0 +1,65 @@
+<template>
+  <div>
+    Google snippet preview:
+    <div class="p-metatags__preview">
+      <div class="p-metatags__google-title">{{ model.title || 'Your title' }}</div>
+      <div class="p-metatags__google-link">yoursite.com/example</div>
+      <div class="p-metatags__google-description">{{ model.description || 'Your description' }}</div>
+    </div>
+    <div class="uk-form-row">
+      <label>Meta Title</label>
+      <input type="text" placeholder="Your title" v-model="model.title" class="uk-width-1-1">
+    </div>
+
+    <div class="uk-form-row">
+      <label>Meta description</label>
+      <textarea rows="4" placeholder="Your description" v-model="model.description" class="uk-width-1-1"></textarea>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  mixins: [window.Storyblok.plugin],
+  methods: {
+    initWith() {
+      return {
+        // needs to be equal to your storyblok plugin name
+        plugin: 'my-plugin-name',
+        title: '',
+        description: ''
+      }
+    },
+    pluginCreated() {
+      // eslint-disable-next-line
+      console.log('View source and customize: https://github.com/storyblok/storyblok-fieldtype')
+    }
+  },
+  watch: {
+    'model': {
+      handler: function (value) {
+        this.$emit('changed-model', value);
+      },
+      deep: true
+    }
+  }
+}
+</script>
+
+<style>
+  .p-metatags__google-title {
+    color: blue;
+    text-decoration: underline;
+  }
+
+  .p-metatags__google-link {
+    color: green;
+  }
+
+  .p-metatags__preview {
+    margin: 5px 0 15px;
+    padding: 10px;
+    color: #000;
+    background: #FFF;
+  }
+</style>
